d9131d35a3562e9b65b2ccda025f88a1ecd1a372
Pratham31patil <pratham31patil@gmail.com>
2022-05-01 19:37:25 +0530

    new changes

--- form.php
+++ form.php
@@ -1,29 +1,34 @@
 <?php
-include('database.php');
-$msg="";
-if(isset($_POST['submit']))
-{
-$title=$_POST['title'];
-$members=$_POST['members'];
-$filename = $_FILES["ppt"]["name"];
+  include('database.php');
+  $msg="";
+  if(isset($_POST['submit']))
+   {
+    $name=$_POST['name'];
+    $enroll=$_POST['enroll'];
+    $email=$_POST['email'];
+    $title=$_POST['title'];
+    $introduction=$_POST['introduction'];
+    $filename = $_FILES["ppt"]["name"];
     $tempname = $_FILES["ppt"]["tmp_name"];    
-        $folder = "uploads/".$filename;
- $filename1 = $_FILES["word"]["name"];
-  $tempname1 = $_FILES["word"]["tmp_name"];    
-         $folder1 = "uploads/".$filename1;
+    $folder = "uploads/".$filename;
+    $filename1 = $_FILES["word"]["name"];
+    $tempname1 = $_FILES["word"]["tmp_name"];    
+    $folder1 = "uploads/".$filename1;
+    $url=$_POST['url'];
+    $query1="INSERT INTO `projectsubmission`(`name`, `enroll_no`, `email`, `title`, `introduction`, `ppt`, `word`, `url`) VALUES ('$name',' $enroll','$email','$title','$introduction','$filename','$filename1','$url')";
+    $run=mysqli_query($con,$query1);
+    if($run){
+        $msg="Thanks,Your response has been submitted ";
+    }
 
-
-$url=$_POST['url'];
-  $query1="INSERT INTO `projectsubmission`(`title`, `members`, `ppt`, `word`, `url`) VALUES ('$title','$members','$filename','$filename1','$url')";
-  $msg="Thanks,Your response has been submitted ";
-  $run=mysqli_query($con,$query1); 
-   move_uploaded_file($tempname, $folder);
+     
+    move_uploaded_file($tempname, $folder);
     
-move_uploaded_file($tempname1, $folder1);
+    move_uploaded_file($tempname1, $folder1);
   
  
 
-	 }
+	}
 
 ?>
 <!DOCTYPE html>
@@ -46,32 +51,47 @@ move_uploaded_file($tempname1, $folder1);
             <div class="col-sm-6 reg">
                 <form class="form-horizontal hr" method="post" enctype="multipart/form-data">
                 <div class="form-group">
-                    <label class="control-label col-sm-4" for="title">Project Title :</label>
-                    <div class="col-sm-8"><input type="text" class="form-control" id="title" name="title" placeholder="Enter project title" required>
+                    <label class="control-label col-sm-4 required"  >Name</label>
+                    <div class="col-sm-8"><input type="text" placeholder="Full Name" class="form-control" name="name" required>
                     </div>
                 </div>
                 <div class="form-group">
-                    <label class="control-label col-sm-4" for="title" >Project Members :</label>
-                    <div class="col-sm-8"><textarea placeholder="Group members ID-Name" class="form-control" id="members" name="members" required></textarea>
+                    <label class="control-label col-sm-4 required" >Enrollment No</label>
+                    <div class="col-sm-8"><input type="text"placeholder="Enroll number" class="form-control" name="enroll" required>
                     </div>
                 </div>
                 <div class="form-group">
-                    <label class="control-label col-sm-4" for="title">Project Ppt :</label>
+                    <label class="control-label col-sm-4 required"  >Email</label>
+                    <div class="col-sm-8"><input type="Email"placeholder="Email" class="form-control" name="email" required>
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label class="control-label col-sm-4 required" >Project Title</label>
+                    <div class="col-sm-8"><input type="text" class="form-control" name="title" placeholder="Enter project title" required>
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label class="control-label col-sm-4"  >Brief Introduction About Project</label>
+                    <div class="col-sm-8"><textarea  class="form-control" name="introduction" required></textarea>
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label class="control-label col-sm-4 required" >Project Ppt</label>
                     <div class="col-sm-8"><input type="file" name="ppt"  class="form-control" id="ppt"required>
                     </div>
                 </div>
                 <div class="form-group">
-                    <label class="control-label col-sm-4" for="title">Project Report :</label>
+                    <label class="control-label col-sm-4  required" >Project Report</label>
                     <div class="col-sm-8"><input type="file" name="word"  class="form-control" id="word"required>
                     </div>
                 </div>
                 <div class="form-group">
-                    <label class="control-label col-sm-4" for="title">Project Github URL :</label>
-                    <div class="col-sm-8"><input type="text" class="form-control" id="url" name="url" placeholder="Enter Github URL">
+                    <label class="control-label col-sm-4 required" > Github URL</label>
+                    <div class="col-sm-8"><input type="text" class="form-control" name="url" placeholder="Enter Github URL" required>
                     </div>
                 </div>    
                 <div class="form-group">
-                    <div class="col-sm-offset-4 col-sm-8"><button type="submit" name="submit" class="btn btn-success">Submit</button>
+                    <div class="col-sm-offset-4 col-sm-8"><button type="submit" name="submit" class="btn btn-success btn-lg">Submit</button>
                     <span style="color:green;"><?php echo $msg?></span>
                 </div>
                 
--- list.php
+++ list.php
@@ -1,12 +1,12 @@
 <?php
-error_reporting(0);
-include_once("database.php");
-$query="select * from projectsubmission";
-$result=mysqli_query($con,$query);
+ error_reporting(0);
+ include_once("database.php");
+ $query="select * from projectsubmission";
+ $result=mysqli_query($con,$query);
 ?>
 <!DOCTYPE html>
-<html lang="en">
-<head>
+ <html lang="en">
+  <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -15,37 +15,43 @@ $result=mysqli_query($con,$query);
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
     <link rel="stylesheet" href="style.css" type="text/css"> 
     <title>Project submited list</title>
-</head>
-<body>
+  </head>
+   <body>
     <div class="table-responsive">
-<table class ="reg register"align="center" border="1px" style="width: 1000px; line-height 500px;">
-    <tr>
-        <th colspan="6"><h1>List of Project submitted</h1></th>
-</tr>
-<tr>
-    <th> #</th>
-    <th>Title</th>
-    <th>Members</th>
-    <th>PPT</th>
-    <th>Word</th>
-    <th>URL</th>
-</tr>
-<?php
-while($rows=mysqli_fetch_assoc($result))
-{
-    ?>
-    <tr>
-    <td><?php echo $rows['no'] ?></td>
-    <td><?php echo $rows['title'] ?></td>
-    <td><?php echo $rows['members'] ?></td>
-    <td><a href="uploads/<?php echo $rows['ppt'] ?>"><?php echo $rows['ppt'] ?></a></td>
-    <td><a href="uploads/<?php echo $rows['word'] ?>"><?php echo $rows['word'] ?></a></td>
-    <td><a href="<?php echo $rows['url'] ?>"><?php echo $rows['url'] ?></a></td>
-</tr>
-<?php
-}
-?>
-</table>
-</div>
-</body>
-</html>
+     <table class ="reg register"align="center" border="1px" style="width: 1000px; line-height 500px;">
+      <tr>
+        <th colspan="9"><h1>List of Project submitted</h1></th>
+      </tr>
+       <tr>
+        <th> #</th>
+        <th>Name</th>
+        <th>Enroll No</th>
+        <th>Email</th>
+        <th>Title</th>
+        <th>Project Introduction</th>
+        <th>PPT</th>
+        <th>Word</th>
+        <th>URL</th>
+      </tr>
+       <?php
+        while($rows=mysqli_fetch_assoc($result))
+        {
+       ?>
+       <tr>
+       <td><?php echo $rows['no'] ?></td>
+       <td><?php echo $rows['name'] ?></td>
+       <td><?php echo $rows['enroll_no'] ?></td>
+       <td><?php echo $rows['email'] ?></td>
+       <td><?php echo $rows['title'] ?></td>
+       <td><?php echo $rows['introduction'] ?></td>
+       <td><a href="uploads/<?php echo $rows['ppt'] ?>"><?php echo $rows['ppt'] ?></a></td>
+       <td><a href="uploads/<?php echo $rows['word'] ?>"><?php echo $rows['word'] ?></a></td>
+       <td><a href="<?php echo $rows['url'] ?>"><?php echo $rows['url'] ?></a></td>
+       </tr>
+        <?php
+        }
+        ?>
+     </table>
+    </div>
+  </body>
+ </html>
